f118d6e6fafcf453533322800ecb0b9c16f8e382
Larry Finkelstein <larry_finkelstein2@comcast.com>
2017-06-01 09:25:10 -0400

    Updates to support running speed tests for all 4 browsers

--- src/main/scala/Speedtest.scala
+++ src/main/scala/Speedtest.scala
@@ -1,47 +1,84 @@
 import com.typesafe.config.{Config, ConfigFactory}
-import org.scalatest._
+import com.typesafe.scalalogging.{LazyLogging, Logger}
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.remote.RemoteWebDriver
+import org.scalatest.{Matchers, concurrent}
 import org.scalatest.selenium.{WebBrowser, _}
-import org.slf4j.{Logger, LoggerFactory}
+import org.scalatest.time.{Seconds, Span}
 
 /**
   * Created by larryf on 5/28/2017.
   */
 
-class Speedtest extends FlatSpec with WebBrowser with concurrent.Eventually {
-}
+object Speedtest extends App with WebBrowser with HtmlUnit with LazyLogging with concurrent.Eventually with Matchers {
 
-object Speedtest extends App with Chrome with WebBrowser {
-
-  val log:Logger = LoggerFactory.getLogger(getClass.getCanonicalName)
+  lazy val os = System.getProperty("os.name")
 
   override def main(args: Array[String]): Unit = {
     Thread.currentThread.setName("ScalaTest-main")
-    println(System.getProperty("os.name"))
 
-    log info s"Hello from $getCurrentDirectory"
+    logger debug s"Hello from $getCurrentDirectory"
 
-    getAllSites.forEach( s =>
-      log info s.getString("name")
-//      runSpeedTests(s)
-    )
-  }
-
-  def runSpeedTests(config: Config) = {
-    //    implicit val patienceConfig =
-    //      PatienceConfig(timeout = scaled(Span(60, Seconds)), interval = scaled(Span(1, Seconds)))
-    // Cancel test when cannot access
-//    try goTo("http://speedtest.xfinity.com") catch { case e: Throwable => cancel(e) }
-    goTo("http://speedtest.xfinity.com")
-    close()
+    getAllDrivers.forEach( d => {
+      logger info s"${d.getString("name")}"
+      if (isOkToRun(d)) {
+        getAllSites.forEach( s => {
+          logger info s"   ${s.getString("name")}"
+          d.getString("name") match {
+            case "Chrome" =>
+              val t = new TestWebSpeedWithChrome
+              t.runSpeedTest(s)
+              t.quit()
+            case "Firefox" =>
+              val t = new TestWebSpeedWithFirefox
+              t.runSpeedTest(s)
+              t.quit()
+            case "Safari" =>
+              val t = new TestWebSpeedWithSafari
+              t.runSpeedTest(s)
+              t.quit()
+            case "InternetExplorer" =>
+              val t = new TestWebSpeedWithSInternetExplorer
+              t.runSpeedTest(s)
+              t.quit()
+            case other =>
+              logger warn  s"Could not find class for $other"
+          }
+        })
+      }
+    })
   }
 
   def getAllSites = {
     val config = ConfigFactory.parseURL(this.getClass.getResource("application.conf"))
     assert(config != null)
-
-    config.getConfigList("sites") // getList("sites")
+    config.getConfigList("sites")
   }
 
-  def getCurrentDirectory = new java.io.File(".").getCanonicalPath
+  def isOkToRun(config: Config): Boolean = {
+    if (config.hasPath("os")) {
+      logger debug s"${config.getString("os")} === $os"
+      if (os.startsWith(config.getString("os"))) true else false
+    } else {
+      true
+    }
+  }
+
+  def getDriver(name: String): RemoteWebDriver = {
+    name match {
+      case "Chrome" => new ChromeDriver()
+      case "Firefox" => new FirefoxDriver()
+    }
+  }
+
+  def getAllDrivers = {
+    val config = ConfigFactory.parseURL(this.getClass.getResource("application.conf"))
+    assert(config != null)
+    config.getConfigList("drivers")
+  }
+
+  def getCurrentDirectory: String = new java.io.File(".").getCanonicalPath
 
 }
