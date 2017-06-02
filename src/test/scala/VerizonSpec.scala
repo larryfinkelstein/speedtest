ef8d6ad80fde653783109515088a4ec6cf214e5a
Larry Finkelstein <larry_finkelstein2@comcast.com>
2017-06-02 15:40:54 -0400

    Additional fixes/testing for all results.

--- src/test/scala/VerizonSpec.scala
+++ src/test/scala/VerizonSpec.scala
@@ -1,3 +1,5 @@
+import java.util
+
 import org.scalatest.selenium._
 import org.scalatest.time.{Seconds, Span}
 import org.scalatest.{FunSpecLike, _}
@@ -11,25 +13,52 @@ trait VerizonSpec extends FunSpecLike with Matchers with concurrent.Eventually {
   describe("www.verizon.com/speedtest/") {
     it("should compute download and upload speeds") {
       implicit val patienceConfig =
-        PatienceConfig(timeout = scaled(Span(60, Seconds)), interval = scaled(Span(1, Seconds)))
+        PatienceConfig(timeout = scaled(Span(120, Seconds)), interval = scaled(Span(1, Seconds)))
       // Cancel test when cannot access
       try goTo("https://www.verizon.com/speedtest/") catch { case e: Throwable => cancel(e) }
+
+      if (webDriver.getClass.getSimpleName.equals("ChromeDriver")) {
+        val tabs2 = new util.ArrayList[String](webDriver.getWindowHandles)
+        webDriver.switchTo().window(tabs2.get(1))
+        webDriver.close()
+        webDriver.switchTo().window(tabs2.get(0))
+      }
+
       eventually {cssSelector("#startButton").element.size.width should be > 0}
       clickOn(cssSelector("#startButton"))
 
-      eventually { cssSelector("#results-download > span.results-type").element.text should be ("DOWNLOAD") }
-      eventually { cssSelector("#results-upload > span.results-type").element.text should be ("UPLOAD") }
+//      eventually { cssSelector("#results-download > span.results-type").element.text should be ("DOWNLOAD") }
+//      eventually { cssSelector("#results-upload > span.results-type").element.text should be ("UPLOAD") }
 
-      println(s"Download: ${cssSelector("#results-download-int").element.text}")
-      println(s"Upload: ${cssSelector("#results-upload-int").element.text}")
+      println("Waiting for test to complete")
+
+      eventually {upDownLength should be > 0}
+
+      println(s"Ping: ${cssSelector("#latency > span").element.text} " +
+        s"${cssSelector("#latency").element.text.split(" ").lastOption.getOrElse("").toLowerCase}")
+      println(s"Download: ${cssSelector("#results-download-int").element.text} " +
+        s"${cssSelector("#results-download > span.value-type").element.text}")
+      println(s"Upload: ${cssSelector("#results-upload-int").element.text} " +
+        s"${cssSelector("#results-upload > span.value-type").element.text}")
+
+      println(s"Location: ${cssSelector("#summary_server_name").element.text}")
+      println(s"IP Address: ${cssSelector("#summary_client_IP").element.text}")
 
       close()
     }
   }
 
+  def upDownLength: Int = {
+    val r = "[0-9\\.]+".r
+    val up = cssSelector("#results-download-int").element.text
+    val dn = cssSelector("#results-upload-int").element.text
+    val l = r.findFirstIn(dn).getOrElse("").length.min(r.findFirstIn(up).getOrElse("").length)
+//    println(s"up=$up dn=$dn l=$l")
+    l
+  }
 }
 
-class VerizonSpecWithChrome extends VerizonSpec with Chrome
-//class VerizonSpecWithSafari extends VerizonSpec with Safari
+class VerizonSpecWithChrome extends VerizonSpec with ChromeBrowser
+class VerizonSpecWithSafari extends VerizonSpec with Safari
 class VerizonSpecWithInternetExplorer extends VerizonSpec with InternetExplorer
 class VerizonSpecWithFirefox extends VerizonSpec with Firefox
--- src/test/scala/XfinitySpec.scala
+++ src/test/scala/XfinitySpec.scala
@@ -1,6 +1,9 @@
+import drivers.ChromeFullScreen
+import org.openqa.selenium.By
 import org.scalatest.selenium._
 import org.scalatest.time.{Seconds, Span}
 import org.scalatest.{FunSpecLike, _}
+import org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated
 
 /**
   * Created by larryf on 5/27/2017.
@@ -15,21 +18,71 @@ trait XfinitySpec extends FunSpecLike with Matchers with concurrent.Eventually {
       // Cancel test when cannot access
       try goTo("http://speedtest.xfinity.com") catch { case e: Throwable => cancel(e) }
 
+      eventually { cssSelector("#action-start-test").element.size.width should be > 0}
       eventually { cssSelector("#action-start-test").element.text should be ("Start Test") }
+
+      println("Clicking GO! button")
       clickOn(cssSelector("#action-start-test"))
 
-      eventually { cssSelector("div.text.text--title").element.text should be ("Speed Results") }
+//      eventually { cssSelector("div.text.text--title").element.text should be ("Speed Results") }
+//
+//      println(s"Download: ${cssSelector("#finalResultsIPv4-download-value").element.text}")
+//      println(s"Upload: ${cssSelector("#finalResultsIPv4-upload-value").element.text}")
 
-      println(s"Download: ${cssSelector("#finalResultsIPv4-download-value").element.text}")
-      println(s"Upload: ${cssSelector("#finalResultsIPv4-upload-value").element.text}")
+      try {
+//        eventually { cssSelector("#action-restart-test").element.text.toUpperCase should be ("Test Again".toUpperCase)}
+//        eventually { cssSelector("#test-results > div > h1").element.text should be ("Speed Results")}
+//        eventually { visibilityOfElementLocated(By.xpath("//button[text()='Test Again']"))}
+//        eventually { cssSelector("#action-advanced-test-settings").element.text should be ("Advanced Test Settings") }
+        eventually {
+          uploadCompleted should be > 0
+//            cssSelector("#action-advanced-test-settings").element.text should be ("Advanced Test Settings")
+        }
 
-      close()
+        val protocol = cssSelector("#finalResults-protocol-version").element.text
+        println(s"Protocol: ${protocol}")
+        protocol match {
+          case "IPv4" =>
+            println(s"Ping: ${cssSelector("#finalResultsIPv4-latency-value").element.text} " +
+              s"${cssSelector("#finalResultsIPv4-latency-unit").element.text}")
+            println(s"Download: ${cssSelector("#finalResultsIPv4-download-value").element.text} " +
+              s"${cssSelector("#final-Results-download-unit").element.text}")
+            println(s"Upload: ${cssSelector("#finalResultsIPv4-upload-value").element.text} " +
+              s"${cssSelector("#finalResults-upload-unit").element.text}")
+          case "IPv6" =>
+            println(s"Ping: ${cssSelector(".finalResultsIPv6-latency-descr #finalResultsIPv6-latency-value").element.text} " +
+              s"${cssSelector(".finalResultsIPv6-latency-descr #finalResultsIPv6-latency-unit").element.text}")
+            println(s"Download: ${cssSelector("#finalResultsIPv6-download-value").element.text} " +
+              s"${cssSelector("#final-Results-download-unit").element.text}")
+            println(s"Upload: ${cssSelector("#finalResultsIPv6-upload-value").element.text} " +
+              s"${cssSelector("#finalResults-upload-unit").element.text}")
+          case _ => println(s"Unknown protocol $protocol")
+        }
+
+        println(s"Location: ${cssSelector("#finalResults-host-location").element.text}")
+//        println(s"IP Address: ${cssSelector(".ip-address").element.text}")
+      } catch {
+        case e: Exception =>
+          println(e.getMessage)
+      }
+      finally
+      {
+        // your scala code here, such as to close a database connection
+        close()
+      }
     }
+  }
+
+  def uploadCompleted: Int = {
+    val x = cssSelector("#finalResultsIPv4-upload-value").element.text
+    val y = cssSelector("#finalResultsIPv6-upload-value").element.text
+//    println(s"$x $y ${x.length + y.length}")
+    x.length + y.length
   }
 
 }
 
-class XfinitySpecWithChrome extends XfinitySpec with Chrome
-//class XfinitySpecWithSafari extends XfinitySpec with Safari
+class XfinitySpecWithChrome extends XfinitySpec with ChromeFullScreen
+class XfinitySpecWithSafari extends XfinitySpec with Safari
 class XfinitySpecWithInternetExplorer extends XfinitySpec with InternetExplorer
 class XfinitySpecWithFirefox extends XfinitySpec with Firefox
