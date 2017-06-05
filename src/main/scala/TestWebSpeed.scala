fd7c8163b26eec106870df4cf1d6e309f8ebf47b
Larry Finkelstein <larry.finkelstein@bms.com>
2017-06-04 20:30:26 -0400

    runSpeedTest now returns a TestResult

--- src/main/scala/TestWebSpeed.scala
+++ src/main/scala/TestWebSpeed.scala
@@ -3,9 +3,8 @@ import java.util
 
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
-import drivers.ChromeFullScreen
+import drivers.{ChromeFullScreen, WebDrivers}
 import models.TestResults
-import org.joda.time.{Interval, Period}
 import org.scalatest.selenium._
 import org.scalatest.time.{Minutes, Seconds, Span}
 import org.scalatest.{Matchers, concurrent}
@@ -13,67 +12,78 @@ import org.scalatest.{Matchers, concurrent}
 /**
   * Created by lfinke200 on 5/31/17.
   */
-trait TestWebSpeed extends LazyLogging with Matchers with concurrent.Eventually {
+trait TestWebSpeed extends LazyLogging with Matchers with concurrent.Eventually with WebDrivers {
   this: WebBrowser with Driver =>
 
-  override implicit val patienceConfig = PatienceConfig(timeout = scaled(Span(2, Minutes)), interval = scaled(Span(1, Seconds)))
+  override implicit val patienceConfig = PatienceConfig(timeout = scaled(Span(3, Minutes)), interval = scaled(Span(1, Seconds)))
 
-  def runSpeedTest(config: Config): Unit = {
-      val t0 = System.nanoTime()
-      try {
+  def runSpeedTest(implicit config: Config): TestResults = {
+    val startTime = System.nanoTime()
+    try {
 
-        startSpeedTest(config)
+      startSpeedTest
 
-        waitForTestToStart(config)
+      waitForTestToStart
 
-        waitForTestToComplete(config)
+      waitForTestToComplete
 
-        val r = getTestResults(config)
+      getTestResults(startTime)
 
-        logger info s"${webDriver.getClass.getSimpleName} ${config.getString("name")} Completed ${formatDuration(t0, System.nanoTime())} s: " +
-          s"${r.protocol.getOrElse("")} ${r.ping} ${r.download}/${r.upload} ${r.location} ${r.ipAddress}"
-      } catch {
-        case e: Throwable =>
-          logger error s"${webDriver.getClass.getSimpleName} ${config.getString("name")} Completed failed ${formatDuration(t0, System.nanoTime())} s: ${e.getMessage}"
-      } finally {
-        quit()
-      }
+    } catch {
+      case e: Throwable =>
+        TestResults(webDriver.getClass.getSimpleName,
+          config.getString("name"),
+          getOsVersion, "failure result", "ping", "dl", "ul", "location", "ipAddress",
+          System.nanoTime() - startTime, None, Some(e.getMessage)
+        )
+    } finally {
+      quit()
     }
+  }
 
-  def startSpeedTest(config: Config): Unit = {
+  def quit: Unit = {
+    webDriver.quit()
+  }
+
+  def startSpeedTest(implicit config: Config): Unit = {
     logger info s"${webDriver.getClass.getSimpleName} ${config.getString("name")} Started - ${config.getString("url")}"
     goTo(config.getString("url"))
   }
 
-  def waitForTestToStart(config: Config): Unit = {
+  def waitForTestToStart(implicit config: Config): Unit = {
     logger info s"   Waiting for ${config.getString("name")} ${config.getString("startButton")}"
     eventually {cssSelector(config.getString("startButton")).element.size.width should be > 0}
     if (config.hasPath("startButtonText")) {
       eventually { cssSelector(config.getString("startButtonTextSelector")).element.text.toUpperCase should be
-        (config.getString("startButtonText").toUpperCase) }
+        config.getString("startButtonText").toUpperCase }
     }
     logger info s"      clicking on ${config.getString("startButtonClick")} button"
     clickOn(cssSelector(config.getString("startButtonClick")))
   }
 
-  private def getTestResults(config: Config): TestResults = {
+  private def getTestResults(startTime: Long)(implicit config: Config): TestResults = {
+    //FIXME: pingSpeedUnit for Verizon needs to be trimmed
     val ping = s"${getSpeedText(config, "pingSpeed")} ${getSpeedText(config, "pingSpeedUnit")}"
     val dl = s"${getSpeedText(config, "downloadSpeed")} ${getSpeedText(config, "downloadSpeedUnit")}"
     val ul = s"${getSpeedText(config, "uploadSpeed")} ${getSpeedText(config, "uploadSpeedUnit")}"
     val location = getSpeedText(config, "location")
     val ipAddress = if (config.hasPath("ipAddress")) {getSpeedText(config, "ipAddress")} else { "" }
     val protocol = if (config.hasPath("protocol")) {getSpeedText(config, "protocol")} else { "" }
-    TestResults(ping, dl, ul, location, ipAddress, Some(protocol))
+    val resultId = if (config.hasPath("resultId")) {cssSelector(config.getString("resultId")).element.text} else "none"
+    TestResults(webDriver.getClass.getSimpleName, config.getString("name"), getOsVersion,
+      resultId, ping, dl, ul, location, ipAddress, System.nanoTime()-startTime,
+      Some(protocol), None)
   }
 
-  private def waitForTestToComplete(config: Config): Unit = {
+  private def waitForTestToComplete(implicit config: Config): Unit = {
     logger debug s"      waiting for test to complete"
     eventually {upDownLength should be > 0}
 
     def upDownLength: Int = {
       val r = "[0-9\\.]+".r
-      r.findFirstIn(getSpeedText(config, "downloadSpeed")).getOrElse("").length.min(r.findFirstIn(getSpeedText(config, "uploadSpeed)"))
-        .getOrElse("").length)
+      val d = r.findFirstIn(getSpeedText(config, "downloadSpeed")).getOrElse("")
+      val u = r.findFirstIn(getSpeedText(config, "uploadSpeed")).getOrElse("")
+      d.length.min(u.length)
     }
   }
 
@@ -94,29 +104,17 @@ trait TestWebSpeed extends LazyLogging with Matchers with concurrent.Eventually 
     }
   }
 
-  private def formatDuration(t0: Long, t1: Long): String = {
-    val interval: Interval = new Interval(t0/1000000, t1/1000000)
-    val period: Period = interval.toPeriod
-    if (period.getHours > 0) {
-      f"${period.getHours}%02d:${period.getMinutes}%02d:${period.getSeconds}%02d.${period.getMillis}"
-    } else {
-      if (period.getMinutes > 0) {
-        f"${period.getMinutes}%02d:${period.getSeconds}%02d.${period.getMillis}"
-      } else {
-        f"${period.getSeconds}%d.${period.getMillis}"
-      }
-    }
-  }
 }
 
 trait ChromeBrowser extends TestWebSpeed with ChromeFullScreen {
-  override def startSpeedTest(config: Config): Unit = {
+  override def startSpeedTest(implicit config: Config): Unit = {
     super.startSpeedTest(config)
     val tabs = new util.ArrayList[String](webDriver.getWindowHandles)
     webDriver.switchTo().window(tabs.get(1))
     webDriver.close()
     webDriver.switchTo().window(tabs.get(0))
   }
+  override def quit: Unit = webDriver.quit()
 }
 
 //class TestWebSpeedWithChrome extends TestWebSpeed with Chrome
