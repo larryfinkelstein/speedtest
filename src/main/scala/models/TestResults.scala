2c4f9bff71ebdf244d60e0af6c9225fe9ccd3557
Larry Finkelstein <larry.finkelstein@bms.com>
2017-06-04 20:30:59 -0400

    More values returned in TestResult

--- src/main/scala/models/TestResults.scala
+++ src/main/scala/models/TestResults.scala
@@ -1,16 +1,20 @@
 package models
 
-import java.util.Date
-
 /**
   * Created by lfinke200 on 6/1/17.
   */
 
 case class TestResults(
+                      browser: String,
+                      site: String,
+                      os: String,
+                      resultId: String,
                       ping: String,
                       download: String,
                       upload: String,
                       location: String,
                       ipAddress: String,
-                      protocol: Option[String]
+                      duration: Long,
+                      protocol: Option[String],
+                      message: Option[String]
                       )
