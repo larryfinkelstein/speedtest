dd38f190fdac393a5fbb6f466620104e36fe5ca1
Larry Finkelstein <larry.finkelstein@bms.com>
2017-06-04 20:39:58 -0400

    Display all test results here

--- src/main/scala/Speedtest.scala
+++ src/main/scala/Speedtest.scala
@@ -1,11 +1,14 @@
 import com.typesafe.config.{Config, ConfigFactory}
 import com.typesafe.scalalogging.LazyLogging
 import drivers.WebDrivers
+import models.TestResults
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.remote.RemoteWebDriver
 import org.scalatest.selenium.{WebBrowser, _}
 import org.scalatest.{Matchers, concurrent}
+
+import scala.collection.mutable.ArrayBuffer
 
 /**
   * Created by larryf on 5/28/2017.
@@ -16,34 +19,35 @@ object Speedtest extends App with WebBrowser with WebDrivers with HtmlUnit with 
   override def main(args: Array[String]): Unit = {
     Thread.currentThread.setName("ScalaTest-main")
 
+    var results = ArrayBuffer[TestResults]()
     getAllDrivers.forEach( d => {
       if (isOkToRun(d)) {
         getAllSites.forEach( s => {
           val browser = d.getString("name")
           logger debug s"$browser ${s.getString("name")}"
-          browser match {
+          val t:TestWebSpeed = browser match {
             case "Chrome" =>
-              val t = new TestWebSpeedWithChrome
-              t.runSpeedTest(s)
-              t.quit()
+              new TestWebSpeedWithChrome
             case "Firefox" =>
-              val t = new TestWebSpeedWithFirefox
-              t.runSpeedTest(s)
-              t.quit()
+              new TestWebSpeedWithFirefox
             case "Safari" =>
-              val t = new TestWebSpeedWithSafari
-              t.runSpeedTest(s)
-              t.quit()
+              new TestWebSpeedWithSafari
             case "InternetExplorer" =>
-              val t = new TestWebSpeedWithSInternetExplorer
-              t.runSpeedTest(s)
-              t.quit()
+              new TestWebSpeedWithSInternetExplorer
             case other =>
               logger warn  s"Could not find class for $other"
+              new TestWebSpeedWithChrome
           }
+          results += t.runSpeedTest(s)
+          t.quit
         })
       }
     })
+    results.foreach(r =>
+      logger info s"${r.browser} ${r.site} Completed ${formatDuration(0,r.duration)} - " +
+        s"id=${r.resultId}-${r.protocol.getOrElse("")} ${r.ping} ${r.download}/${r.upload} " +
+        s"${r.location} ${r.ipAddress} ${r.message.getOrElse("")}"
+    )
   }
 
   def getAllSites = {
